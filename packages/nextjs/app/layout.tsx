8e09948a0767db5e00002599c1b9a68e99156e42
Chris-Armel <817573+daqhris@users.noreply.github.com>
2024-07-23 15:47:28 +0200

    modify app titles

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,8 +5,8 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
+  title: "ZinnekeRescueMission App",
+  description: "Built with 🏗 Scaffold-ETH 2 during ETHGlobal Brussels 2024",
 });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
